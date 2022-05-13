4f58b1964c4404f2f94b17115f4c54959206b757
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-05-13 09:34:20 -0400

    Insert element in a index

--- LinkedList/Linkedlist/Linkedlist/Program.cs
+++ LinkedList/Linkedlist/Linkedlist/Program.cs
@@ -25,7 +25,7 @@ namespace Linkedlist
             listSize = 0;
         }
 
-       public void  addEnd(LinkedListNode inputNode)
+        public void addEnd(LinkedListNode inputNode)
         {
             LinkedListNode node = inputNode;
             LinkedListNode current;
@@ -36,44 +36,83 @@ namespace Linkedlist
                 //this.head = node;
                 this.head = inputNode;
 
-                
-            }else
+
+            }
+            else
             {
                 //if the list(head) has already value, we start traversing the list from the beginning(head)
                 current = head;
 
-                while(current.next != null)
+                while (current.next != null)
                 {
                     //current take the value from next element
                     current = current.next;
-                    
+
                 }
 
                 //Add to current variable which contain the  last element,  the next node in its property next
                 current.next = inputNode;
-                
+
             }
 
             //increase size with ++
             listSize++;
 
         }
+        //Insert an element at a specific location
+        public bool insert(int position, LinkedListNode inputNode)
+        {
+            //if it is not valid range
+            if (position < 0 || position > listSize)
+            {
+                return false;
+            }
+
+            LinkedListNode current = head;
+            LinkedListNode previous = null;
+            int index = 0;
+
+            //Agregar un elemento al inicio 
+            if (position == 0)
+            {
+                inputNode.next = current;
+                head = inputNode;
+            }
+            else
+            {
+                //loop through the list to the specified position
+                while (index++ < position)
+                {
+                    previous = current;
+                    current = current.next;
+                }
+
+                inputNode.next = current;
+                previous.next = inputNode;
+
+            }
+
+            listSize++;
+            return true;
+
+        }
+
        
 
         public void printList()
         {
             LinkedListNode current = head;
-            while(current.next != null)
+            while (current.next != null)
             {
                 Console.WriteLine(current.data);
                 current = current.next;
-               
+
             }
             //last element
             Console.WriteLine(current.data);
         }
 
-        
+
     }
 
 
@@ -85,14 +124,18 @@ namespace Linkedlist
 
             LinkedListNode node = new LinkedListNode(1);
             LinkedListNode node2 = new LinkedListNode(2);
-          
+            LinkedListNode node3 = new LinkedListNode(3);
 
             LinkedList list = new LinkedList();
             list.addEnd(node);
             list.addEnd(node2);
             list.printList();
             Console.WriteLine("\n");
-          
+            //Insert specific index
+            list.insert(2, node3);
+            list.printList();
+            Console.WriteLine("\n");
+         
 
         }
     }
